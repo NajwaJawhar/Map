7de54e095500d714eb4146eb69d2aa9329618d0f
Najwa <jawhar.najwaa@gmail.com>
2023-11-21 09:14:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+        setContentView(R.layout.activity_main);
+    }
+}
